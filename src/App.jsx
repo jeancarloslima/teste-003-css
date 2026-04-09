2c7068556f5a9a593e7da04b36132b443a29bde8
Jean Lima <jcoliveiralima19@gmail.com>
2026-04-09 12:29:03 -0300

    Fazendo exercício de componente de botão com Tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import Tela01 from './components/Tela01'
 import Tela02 from './components/Tela02'
 import Tela03 from './components/Tela03'
+import Tela04 from './components/Tela04'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Tela02 type="sucess" />
       <Tela02 type="error" />
       <Tela03 />
+      <Tela04 texto="Clique aqui!" />
     </>
   )
 }
---
+++ src/components/Tela04/index.jsx
@@ -0,0 +1,7 @@
+export default function Tela04({ texto }) {
+    return (
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded trasinsion colors cursor-pointer">
+            {texto}
+        </button>
+    )
+}
